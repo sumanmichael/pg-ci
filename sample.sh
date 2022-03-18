6882583204193351bf4af43f49dfdfea9149d811
Suman Michael <sumanmichael01@gmail.com>
2022-03-18 19:07:02 +0530

    demo run

--- sample.sh
+++ sample.sh
@@ -1,11 +1,6 @@
 psql -c 'SELECT version();'
-
 psql -c 'CREATE DATABASE dvdrental;'
-
 pg_restore -U postgres -d dvdrental ./dvdrental.tar
-
 psql -d dvdrental -f ./create_func.sql
-
 psql -d dvdrental -c 'CREATE EXTENSION plpgsql_check;'
-
 psql -d dvdrental -c "select * from plpgsql_check_function_tb('find_usable_indexes');" -o /tmp/plpgsql_check_result.txt
