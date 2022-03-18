cc480352a749e54aa6e752912356d04b04cbf890
Suman Michael <sumanmichael01@gmail.com>
2022-03-18 16:41:47 +0530

    install plpgsql check

--- sample.sh
+++ sample.sh
@@ -5,3 +5,7 @@ psql -c 'CREATE DATABASE dvdrental;'
 pg_restore -U postgres -d dvdrental ./dvdrental.tar
 
 psql -d dvdrental -c 'select count(*) from film;'
+
+psql -d dvdrental -c 'CREATE EXTENSION plpgsql_check;'
+
+psql -d dvdrental -c "select * from plpgsql_check_function_tb('film_in_stock');"
