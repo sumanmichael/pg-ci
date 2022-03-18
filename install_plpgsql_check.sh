d44b8fb518e255f78180a154acea8f544e66bea6
Suman Michael <sumanmichael01@gmail.com>
2022-03-18 16:47:33 +0530

    added pgdg

--- install_plpgsql_check.sh
+++ install_plpgsql_check.sh
@@ -1,4 +1,7 @@
-sudo apt-get install postgresql-server-dev-14 libicu-dev gcc make
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt -y update
+sudo apt-get install -y postgresql-server-dev-14 libicu-dev gcc make
 git clone --depth 1 https://github.com/okbob/plpgsql_check.git 
 cd plpgsql_check/
 sudo make && sudo make install
