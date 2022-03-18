7a7b72633586e091bc70fe69b0acbf02c12a376f
Suman Michael <sumanmichael01@gmail.com>
2022-03-18 16:42:51 +0530

    pg version fix

--- install_plpgsql_check.sh
+++ install_plpgsql_check.sh
@@ -1,4 +1,4 @@
-sudo apt-get install postgresql-server-dev-11 libicu-dev gcc make
+sudo apt-get install postgresql-server-dev-14 libicu-dev gcc make
 git clone --depth 1 https://github.com/okbob/plpgsql_check.git 
 cd plpgsql_check/
 sudo make && sudo make install
